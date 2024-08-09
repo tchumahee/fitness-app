dd2d5ac82a70049619ef6fe4e5697e663daf5542
Sofija Rodić <74682145+Sofija1234@users.noreply.github.com>
2024-08-09 13:47:08 +0200

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule),
+    // canActivate: [GuardService]
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
