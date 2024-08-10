953c9756879a834930e964484c9de677261da176
Sofija Rodić <74682145+Sofija1234@users.noreply.github.com>
2024-08-10 23:40:03 +0200

    Login form with redirect

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  public form: FormGroup = new FormGroup({});
 
+  constructor(
+    //private loginService: LoginService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder, 
+    ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required]
+    });
+  }
+
+  public login(form: any) {
+    this.router.navigate(['home']);
+    // if (this.loginService.login(form.value.username, form.value.password)) {
+    //   this.router.navigate(['events']);
+    // } else {
+    //   this.snackBar.open("Podaci nisu dobri", undefined, {
+    //     duration: 2000
+    //   })
+    // }
+  }
 }
