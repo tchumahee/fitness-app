2012dc0a0c0350bb3ba4a58ba5c30eebbeef60c0
Sofija Rodić <74682145+Sofija1234@users.noreply.github.com>
2024-08-29 21:02:45 +0200

    Update user-programs.module.ts

--- src/app/user-programs/user-programs.module.ts
+++ src/app/user-programs/user-programs.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { UserProgramsRoutingModule } from './user-programs-routing.module';
 import { UserProgramsViewComponent } from './user-programs-view/user-programs-view.component';
-import { ProgramCardComponent } from '../fitness-programs/program-card/program-card.component';
 import { FitnessProgramsModule } from '../fitness-programs/fitness-programs.module';
 
 
